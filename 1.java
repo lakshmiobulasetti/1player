fe52333a09740e96be96d2ad721dd8938e55d5fb
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-11 10:16:35 +0530

    Update and rename README.md to 1.java

---
+++ 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String string="luckyraj";
+           String reverse = new StringBuffer(string).reverse().toString();
+           System.out.println("\nString before reverse: "+string);
+           System.out.println("String after reverse: "+reverse);
+	}
+}
